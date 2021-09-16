3722d9916c82f6dfab6f0edf390e65c10b00bb1a
afsha30 <86714813+afsha30@users.noreply.github.com>
2021-09-16 09:34:00 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Hello
+{
+                 public static void main(String args[])
+                 {
+                            Scanner scob = new Scanner (System. in) ;
+                            String str="abCDefGH";
+                            String lower=str.toLowerCase ( ) ;
+                            String upper=str.toUpperCase();
+
+                             System.out.println(str) ;
+                             System.out.println(lower);
+                             System.out.println(upper);
+                }
+ }
