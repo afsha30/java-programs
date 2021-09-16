af23984b1503e0c868ff2175dafab5a052def708
afsha30 <86714813+afsha30@users.noreply.github.com>
2021-09-16 09:33:28 +0530

    Add files via upload

---
+++ Alpha.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Alpha
+{
+       public static void main(String args[])
+       {
+             Scanner scob=new Scanner(System.in);
+             String str=scob.nextLine();
+
+             String word=" ";
+             for(int i=0;i<str.length();i++)
+             {
+                     if(str.charAt(i)==' ' || str.charAt(i)=='.')
+                     {
+                            int f=str.indexOf(word);
+                            int l=str.lastIndexOf(word);
+                            if(f==l)
+                                    System.out.println(word);
+                            word=new String();
+                      }
+                      else
+                             word=word+str.charAt(i);
+             }
+        }
+}
